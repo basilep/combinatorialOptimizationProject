59c86ae308274a98d147d11174bb0e4435beff88
basilep <basile.perreaux@student.unamur.be>
2022-05-09 14:39:18 +0200

    get_child_info with lb and sol_ehuritic ok

--- BB.py
+++ BB.py
@@ -3,13 +3,17 @@ import math
 import copy
 from pickle import TRUE
 from unittest import result
+
+"""
+Compute the lower bound of a node
+"""
 def compute_lb(capacity,product,node):
     if len(product)!=0:
         lowest_product=min(product)
     else:
         for sac in node:
             lowest_product=min(sac)
-    # print("lowest_product is",lowest_product)
+
     j1_bound=capacity-lowest_product
     j2_bound=capacity/2
     j3_bound=lowest_product
@@ -34,18 +38,17 @@ def compute_lb(capacity,product,node):
             j2.append(super_item)
         elif super_item>=j3_bound and super_item <=j2_bound:
             j3.append(super_item)
-        # print("super_item is",super_item)
-    # print("actuel_node is",node)
-    # print("length of j1,j2,j3:",len(j1),len(j2),len(j3))
-    lb=len(j1)+len(j2)+max(0,(sum(j3)-(len(j2)*capacity-sum(j2)))/capacity)
-    #print("lower_bound is",math.ceil(lb))
-    return math.ceil(lb)
 
+    lb=len(j1)+len(j2)+max(0,(sum(j3)-(len(j2)*capacity-sum(j2)))/capacity)
+    
+    return math.ceil(lb)
+"""
+Compute the heurisical solution from a node
+"""
 def heuristic_local(capacity, product,node):
-    #setup all variables for loops
+    # Setup all variables for loops
     binCount = len(node)
     bin = [capacity] * (len(product)+binCount)
-    # print(bin)
     i=0
     for sac in node:  
         super_item=0    
@@ -53,161 +56,141 @@ def heuristic_local(capacity, product,node):
             super_item+=j
 
         bin[i]=capacity-super_item
-        # print(bin[i])
         i+=1
-    # print(bin)
 
-    #for loop goes through each item to place the item into a bin
+    # For loop goes through each item to place the item into a bin
     for item in range(len(product)):
         index = 0
-        #while loop finds the bin to place the item into based on space
-        #and what's open
+        # While loop finds the bin to place the item into based on space
+        # And what's open
         while(index < binCount):
             if(bin[index] >= product[item]):
                 bin[index] = bin[index] - product[item]
                 break
             index += 1
-        #If there wasn't a bin for the item to go into, open a new one
+        # If there wasn't a bin for the item to go into, open a new one
         if(index == binCount):
             bin[binCount] = capacity - product[item]
             binCount += 1
-
-
-    #print("solution heuristique : ", binCount)    
+   
     return binCount
 
-#heuristic_local(100,[26,26,22,20,19],[[49,41],[34,33],[29]])
-
+"""
+Get all possible node from the left boxes
+"""
 def forward_left(capacity,product,node_actuel):
-    node_left=node_actuel
+    node_left=copy.deepcopy(node_actuel)
+    node_left_ensemble=[]
     new_product=product[0]
-    # print("actual is",node_actuel)
-    not_put_yet=True
-    for sac in node_left:  
+
+    for sac in node_left: 
         super_item=0    
         for j in sac:
             super_item+=j
-        if super_item+new_product<=capacity and not_put_yet==True:
-            sac.append(new_product)
-            not_put_yet=False
+        if super_item+new_product<=capacity:
+            sac.append(new_product)   #sac est juste un 
+            node_left1=copy.deepcopy(node_left)
+            node_left_ensemble.append(node_left1)
+            sac.pop()
 
-    #print("left is",node_left)
-    return node_left
-
-def forward_right(capacity,product,node_actuel):
+    return node_left_ensemble
+"""
+Get the node right
+"""
+def forward_right(product,node_actuel):
     node_right=node_actuel
     new_product=product[0]
     new_bag=[new_product]
-    # print("actual is",node_actuel)
     node_right.append(new_bag)
-    #print("right is",node_right)
     return node_right
-    
-def forward(capacity,product,node_actuel):
-    print("actual is",node_actuel)
-    forward_right(capacity,product,node_actuel)
-    forward_left(capacity,product,node_actuel)
-    
 
-# forward_left(100,[34,33,29,26,26,22,20,19],[[49],[41]])
-#forward_right(100,[34,33,29,26,26,22,20,19],[[49],[41]])
-#forward(100,[34,33,29,26,26,22,20,19],[[49],[41]])
-#compute_lb(100,[29,26,26,22,20,19],[[49],[41,33],[34]])
 
 def branch_and_bound(instance_name, branching_scheme ,valid_inequalities, time_limit):
     return True
-""" 
-def leftScheme(capacity, product, testNode):
-    if(product==[]):
-        print(testNode)
-        return testNode
-    else:
-        #Precalculate left node if it's possible
-        actual_node = copy.deepcopy(testNode)
-        left = forward_left(capacity,product, testNode)
-        #compute_lb(capacity, product, actual_node)
-        if(left == actual_node):
-            return leftScheme(capacity, product[1:], forward_right(capacity, product, actual_node))
-        else:
-            return leftScheme(capacity, product[1:], left)
-   
-def test(capacity, product, testNode):
-    if(product==[]):
-        return True
-    else:
-        firstProduct = product[0]
-        node = copy.deepcopy(testNode)
-        resultLeft = leftScheme(capacity, product, testNode)
-        compute_lb(capacity, product, testNode)
-        heuristic_local(capacity, product, node)
-        
-        test(capacity, product[1:], forward_right(capacity, product, node))
 
+"""
+Calculate all the possible child node with their lower bound and their heuristic solution
 
-def leftThanRight(capacity, product):
-    if(product==[]):
+capacity = capacity of the boxes
+product = the product that are not already in a box
+node_actuel = node from where the child are calculated (product already in boxes)
+
+return result which is a dictionnary containing the different nodes with their lower bound and heuristic solution
+{i: [[node], lb, heur_sol]}
+
+"""
+def get_child_node(capacity, product, node_actuel):
+    if(product == []):
         return False
     else:
-        productAllreadyPast = []
-        result = []
-        while(len(product)>0):
-            left = leftScheme(capacity, product, [])
-            result.append(productAllreadyPast + left)
-            productAllreadyPast.append([product[0]])
-            product = product[1:]
-            
-        print("\n\n")
-        for elem in result:
-            print(elem)"""
-
-def get_child_node(capacity,product,node_actuel):
-    if(product==[]):
-        return False
-    else:
-        actual_lower_bound = compute_lb(capacity,product, node_actuel)
-        #print("Actual node", node_actuel)
-        #print("actual_lb", actual_lower_bound)
-
         # Get left child
         # Need to deepcopy, otherwise, the forward_left change the node_actuel variable
-        left = forward_left(capacity,product, copy.deepcopy(node_actuel))
-        left_lower_bound = compute_lb(capacity,product[1:], left)
-        left_heuristic = heuristic_local(capacity,product[1:], left)
-        #print("left", left)
-        #print("left_lb", left_lower_bound)
-
-        # Get right child
-        right = forward_right(capacity,product, copy.deepcopy(node_actuel))
-        right_lower_bound = compute_lb(capacity,product[1:], right)
-        right_heuristic = heuristic_local(capacity,product[1:], right)
-        #print("right", right)
-        #print("right_lb", right_lower_bound)
-
-    return {"left" : left, "left_lb" :left_lower_bound, "left_sol":left_heuristic, "right": right, "right_lb": right_lower_bound, "right_sol":right_heuristic}
-
-def get_child_info(capacity,product,node_actuel):
-    if(product==[]):
+        left_set = forward_left(capacity, product, copy.deepcopy(node_actuel))
+        result = {}
+        left_id = 0
+        for left_id in range(len(left_set)):
+            result[left_id] = [left_set[left_id], compute_lb(capacity, product[1:], left_set[left_id]), heuristic_local(capacity, product[1:], left_set[left_id])]
+        right = forward_right(product, copy.deepcopy(node_actuel))
+        result[left_id+1] = [right, compute_lb(capacity, product[1:], right), heuristic_local(capacity, product[1:], right)]
+    return result
+"""
+Get all the sub tree from a node
+"""
+def get_child_info(capacity, product, node_actuel):
+    if(product == []):
         total_result.append(node_actuel)
-        #print(node_actuel)
-        return False
     else:
-        child_info = get_child_node(capacity,product,node_actuel)
-        print("\n")
+        child_info = get_child_node(capacity, product, node_actuel)
         for key, val in child_info.items():
-            print(key,":", val)
-        if(child_info.get("left")!= node_actuel):
-            get_child_info(capacity,copy.deepcopy(product[1:]),copy.deepcopy(child_info.get("left")))
-        get_child_info(capacity,copy.deepcopy(product[1:]),copy.deepcopy(child_info.get("right")))
-        
-        
-#forward_left(100,[34,33,29,26,26,22,20,19], [[49,41]])
-#leftThanRight(100,[49, 41, 34,33,29,26,26,22,20,19])
+            get_child_info(capacity, product[1:], val[0])
 
-#leftScheme(100,[49, 41, 34,33,29,26,26,22,20,19],[])
+def test(capacity, product, node_actuel):
+    if(product == []):
+        total_result.append(node_actuel)
+    else:
+        child_info = get_child_node(capacity, product, node_actuel)
+        lower_bounds=[]
+        lower_key = []
+        sol_heuristic=[]
+        sol_heur_key = []
+        for key, val in child_info.items():
+            lower_bounds.append(val[1])
+            lower_key.append(key)
+            sol_heuristic.append(val[2])
+            sol_heur_key.append(key)
+        print(sol_heuristic)
+        #print(lower_bounds)
+        #print(sol_heuristic)
+        #print(node_actuel)
+        lb_min = min(sol_heuristic)
+        for min_id in range(len(sol_heuristic)):
+            if sol_heuristic[min_id] == lb_min:
+                print(child_info)
+                print(min_id)
+                #print(lb_min)
+                #print(child_info.get(min_id)[0])
+                test(capacity, product[1:], child_info.get(min_id)[0])
+        index_min_lb = lower_bounds.index(min(lower_bounds))
+        index_min_sol = sol_heuristic.index(min(sol_heuristic))  
+
 total_result = []
-get_child_info(100,[34,33,29,26,26,22,20,19], [[49], [41]])
-print(len(total_result))
-"""for elem in total_result:
-    print(len(elem))"""
 
-#print(forward_left(100, [34,33,29,26,26,22,20,19], [[49, 41]]))
+get_child_info(100, [49, 41, 34, 33, 29, 26, 26, 22, 20, 19], [])
+
+min_boxes = 10 #Changer en len(product) par la suite
+supp = []
+
+for elem in total_result:
+    if len(elem) < min_boxes:
+        min_boxes = len(elem)
+    """
+    print(elem)
+    if elem not in supp:
+        supp.append(elem)"""
+for elem in total_result:
+    if len(elem) == min_boxes:
+        print(elem)
+        print(len(elem))
+
+
+print(len(total_result))
--- Instances/bp.py
+++ Instances/bp.py
@@ -5,14 +5,31 @@ import math
 import os
 import time
 import pandas
+import copy
 from IPython.display import display
 
+################################
+###           GLPK           ###
+################################
+
+# Globals variable
+# List to display the result in a DataFrame
+result_set = []
+result_id = 0
+
+# List for branch and bound
+total_result = []
+"""
+Allow to display the results in a dataframe and in detail file
+"""
 def display_result(tuple_obj_x_y):
+    global result_set
+    global result_id
     if(tuple_obj_x_y == None):
-        d = open("details.txt", "a")
+        d = open("details_glpk.txt", "a")
         d.write("\nFail\n\n")
-        result_set[i]["solution"] = "not found"
-        result_set[i]["result"] = "fail"
+        result_set[result_id]["solution"] = "not found"
+        result_set[result_id]["result"] = "fail"
         d.close()
     else:
         details_data = []
@@ -20,7 +37,7 @@ def display_result(tuple_obj_x_y):
         print("Number of boxes used: ",obj)
         x = tuple_obj_x_y[1]
         y = tuple_obj_x_y[2]
-        d = open("details.txt", "a")
+        d = open("details_glpk.txt", "a")
         d.write("\n")
         for b in range(0, len(y)):
             details_data.append({"product":"", "box":""})
@@ -36,14 +53,16 @@ def display_result(tuple_obj_x_y):
                 #remove empty boxes
                 clean_data = [elem for elem in ({key: val for key, val in sub.items() if val} for sub in details_data) if elem]
                 
-        result_set[i]["solution"] = int(obj)
-        result_set[i]["result"] = "success"
+        result_set[result_id]["solution"] = int(obj)
+        result_set[result_id]["result"] = "success"
         df_detail = pandas.DataFrame(clean_data, columns=['box', 'product'])
         df_detail = df_detail.set_index('box')
         #display(df_detail)
         d.write(str(df_detail)+"\n\n")
         d.close()
-
+"""
+Get product in a dictionnary from an instance file (from the statement)
+"""
 def get_products_parameter(all_products):
     result_dictionnary = {}
     for elem in all_products:
@@ -54,33 +73,37 @@ def get_products_parameter(all_products):
             result_dictionnary[int(tmp[4])] = int(tmp[5][:len(tmp[5])-1])
         
     return result_dictionnary
-
+"""
+Get the physical minimum of boxes to use
+"""
 def minimum_boxes(capacity, data):
     weight=0
     for elem in data.values():
         weight+=elem
     return int(math.ceil(weight / capacity))
-
+"""
+Solve the bin packing problem from an instance
+Return a tuple containing:
+- the optimal value (obj)
+- an optimal solution with a list of x[p][b] = xpb and a list of y[b] = yb
+"""
 def solve_bp_lp(instance_name):
-    #Open the instance's file
-    instanceFile = open(instance_name, "r")
-    #Read the file and store every line in a list
-    data_file = instanceFile.readlines()
-    #Close the file
-    instanceFile.close()
+    global result_set
+    global result_id
+    data_file = get_data_instance(instance_name)
     #Get the box capacity from the file's data
     box_capacity = data_file[2].split(":= ")[1]
     #Remove the ";"
     box_capacity = box_capacity[:len(box_capacity)-2]
-    result_set[i]["c"] = box_capacity
+    result_set[result_id]["c"] = box_capacity
     product_parameters= get_products_parameter(data_file[5:])
     number_of_products = len(product_parameters)
-    result_set[i]["n"] = number_of_products
+    result_set[result_id]["n"] = number_of_products
     #print(product_parameters)
     #print("Number of products: ",number_of_products)
     #print("Box capacity: "+box_capacity)
     #print("Size of the different products:", list(product_parameters.values()))
-    result_set[i]["lb"] = minimum_boxes(int(box_capacity), product_parameters)
+    result_set[result_id]["lb"] = minimum_boxes(int(box_capacity), product_parameters)
     #print("Physical minimum number of boxes to use:", minimum_boxes(int(box_capacity), product_parameters))
     solver = po.SolverFactory('glpk') # GNU Linear Programming Kit
     #Create the model
@@ -113,7 +136,7 @@ def solve_bp_lp(instance_name):
         model.product_in_only_one_box.add(lhs == rhs)
 
     #Solver
-    solver.solve(model, timelimit=10)    #To get the details : tee=True
+    solver.solve(model, timelimit=600)    #To get the details : tee=True
     try:
         #Results
         obj = pe.value(model.obj)
@@ -126,48 +149,260 @@ def solve_bp_lp(instance_name):
         return (obj, x, y)
     except:
         return None
+"""
+Solve the linear relaxation problem with glpk
+For all instances in the current folder
+Store the result in result_glpk.txt
+and the details of the products in box in details_glpk.txt
+"""
+def launch_glpk():
+    global result_id
 
-#display_result(solve_bp_lp("./bin_pack_20_0.dat"))
-#Gets the files names from the directory  
-instances = os.listdir(".")
-if "bp.py" in instances:
-    instances.remove("bp.py")
-if "result.txt" in instances:
-    instances.remove("result.txt")
-if "details.txt" in instances:
-    instances.remove("details.txt")
-if "BFD.py" in instances:
-    instances.remove("BFD.py")
-if "opentool.py" in instances:
-    instances.remove("opentool.py")
-if "test.txt" in instances:
-    instances.remove("test.txt")
+    f = open("result_glpk.txt", "w")
+    f.close()
+    d = open("details_glpk.txt", "w")
+    d.close
     
+    instances = get_instances()
+    for instance in instances[33:]:
+        d = open("details_glpk.txt", "a")
+        d.write(instance)
+        d.close()
+        print(instance)
+        result_set.append({"file":instance})
+        time_before = time.time()
+        try:
+            display_result(solve_bp_lp("./"+instance))
+        except:
+            print("ERROR")
+        time_after = time.time()
+        print("The solve lasts ", round(time_after-time_before, 2), "seconds\n")  #Round the float to 2 digits after the comas
+        result_set[result_id]["time"] = round(time_after-time_before, 2)
+        result_id += 1
+    df = pandas.DataFrame(result_set, columns=['file', 'c', 'n', 'lb', 'solution', 'result', 'time'])
+    print(df.to_string())
+    f = open("result_glpk.txt", "a")
+    f.write(df.to_string())
+    f.close()
 
-f = open("result.txt", "w")
-f.close()
-d = open("details.txt", "w")
-d.close()
-#List to display the result in a DataFrame
-result_set = []
-i=0
-for instance in instances:
-    d = open("details.txt", "a")
-    d.write(instance)
-    d.close()
-    print(instance)
-    result_set.append({"file":instance})
-    time_before = time.time()
-    try:
-        display_result(solve_bp_lp("./"+instance))
-    except:
-        print("ERROR")
-    time_after = time.time()
-    print("The solve lasts ", round(time_after-time_before, 2), "seconds\n")  #Round the float to 2 digits after the comas
-    result_set[i]["time"] = round(time_after-time_before, 2)
-    i+=1
-df = pandas.DataFrame(result_set, columns=['file', 'c', 'n', 'lb', 'solution', 'result', 'time'])
-print(df.to_string())
-f = open("result.txt", "a")
-f.write(df.to_string())
-f.close()
+#################################
+###     Branch and bound      ###
+#################################
+
+"""
+Compute the lower bound of a node
+"""
+def compute_lb(capacity,product,node):
+    if len(product)!=0:
+        lowest_product=min(product)
+    else:
+        for sac in node:
+            lowest_product=min(sac)
+
+    j1_bound=capacity-lowest_product
+    j2_bound=capacity/2
+    j3_bound=lowest_product
+    j1=[]
+    j2=[]
+    j3=[]
+    for i in product:
+        if i>j1_bound:
+            j1.append(i)
+        elif i>j2_bound and i <=j1_bound:
+            j2.append(i)
+        elif i>=j3_bound and i <=j2_bound:
+            j3.append(i)
+    
+    for sac in node:  
+        super_item=0    
+        for j in sac:
+            super_item+=j
+        if super_item>j1_bound:
+            j1.append(super_item)
+        elif super_item>j2_bound and super_item <=j1_bound:
+            j2.append(super_item)
+        elif super_item>=j3_bound and super_item <=j2_bound:
+            j3.append(super_item)
+
+    lb=len(j1)+len(j2)+max(0,(sum(j3)-(len(j2)*capacity-sum(j2)))/capacity)
+    
+    return math.ceil(lb)
+"""
+Compute the heurisical solution from a node
+"""
+def heuristic_local(capacity, product,node):
+    # Setup all variables for loops
+    binCount = len(node)
+    bin = [capacity] * (len(product)+binCount)
+    i=0
+    for sac in node:  
+        super_item=0    
+        for j in sac:
+            super_item+=j
+
+        bin[i]=capacity-super_item
+        i+=1
+
+    # For loop goes through each item to place the item into a bin
+    for item in range(len(product)):
+        index = 0
+        # While loop finds the bin to place the item into based on space
+        # And what's open
+        while(index < binCount):
+            if(bin[index] >= product[item]):
+                bin[index] = bin[index] - product[item]
+                break
+            index += 1
+        # If there wasn't a bin for the item to go into, open a new one
+        if(index == binCount):
+            bin[binCount] = capacity - product[item]
+            binCount += 1
+   
+    return binCount
+
+"""
+Get all possible node from the left boxes
+"""
+def forward_left(capacity,product,node_actuel):
+    node_left=copy.deepcopy(node_actuel)
+    node_left_ensemble=[]
+    new_product=product[0]
+
+    for sac in node_left: 
+        super_item=0    
+        for j in sac:
+            super_item+=j
+        if super_item+new_product<=capacity:
+            sac.append(new_product)   #sac est juste un 
+            node_left1=copy.deepcopy(node_left)
+            node_left_ensemble.append(node_left1)
+            sac.pop()
+
+    return node_left_ensemble
+"""
+Get the node right
+"""
+def forward_right(product,node_actuel):
+    node_right=node_actuel
+    new_product=product[0]
+    new_bag=[new_product]
+    node_right.append(new_bag)
+    return node_right
+
+
+def branch_and_bound(instance_name, branching_scheme ,valid_inequalities, time_limit):
+    return True
+
+"""
+Calculate all the possible child node with their lower bound and their heuristic solution
+
+capacity = capacity of the boxes
+product = the product that are not already in a box
+node_actuel = node from where the child are calculated (product already in boxes)
+
+return result which is a dictionnary containing the different nodes with their lower bound and heuristic solution
+{i: [[node], lb, heur_sol]}
+
+"""
+def get_child_node(capacity, product, node_actuel):
+    if(product == []):
+        return False
+    else:
+        # Get left child
+        # Need to deepcopy, otherwise, the forward_left change the node_actuel variable
+        left_set = forward_left(capacity, product, copy.deepcopy(node_actuel))
+        result = {}
+        left_id = 0
+        for left_id in range(len(left_set)):
+            result[left_id] = [left_set[left_id], compute_lb(capacity, product[1:], left_set[left_id]), heuristic_local(capacity, product[1:], left_set[left_id])]
+        right = forward_right(product, copy.deepcopy(node_actuel))
+        result[left_id+1] = [right, compute_lb(capacity, product[1:], right), heuristic_local(capacity, product[1:], right)]
+    return result
+"""
+Get all the sub tree from a node
+"""
+def get_child_info(capacity, product, node_actuel):
+    if(product == []):
+        total_result.append(node_actuel)
+    else:
+        child_info = get_child_node(capacity, product, node_actuel)
+        lower_bounds=[]
+        sol_heuristic=[]
+        for key, val in child_info.items():
+            lower_bounds.append(val[1])
+            sol_heuristic.append(val[2])
+        lb_min = min(lower_bounds)
+        sol_heu_min = min(sol_heuristic)
+        for min_id in range(len(sol_heuristic)):
+            if (sol_heuristic[min_id] == sol_heu_min) and (lower_bounds[min_id] == lb_min):
+                try:
+                    get_child_info(capacity, product[1:], child_info.get(min_id)[0])
+                except:
+                    get_child_info(capacity, product[1:], child_info.get(min_id+1)[0])
+"""
+
+"""
+def bb(instance):
+    data_file = get_data_instance(instance)
+    #Get the box capacity from the file's data
+    box_capacity = data_file[2].split(":= ")[1]
+    #Remove the ";"
+    box_capacity = box_capacity[:len(box_capacity)-2]
+    product_parameters=get_products_parameter(data_file[5:])
+    product_list = [int(product) for product in list(product_parameters.values())]
+    get_child_info(int(box_capacity), product_list, [])
+"""
+PAS ENCORE OK
+"""
+def launch_branch_and_bound():
+    global total_result
+    
+    for elem in total_result:
+        if len(elem) < min_boxes:
+            min_boxes = len(elem)
+    for elem in total_result:
+        if len(elem) == min_boxes:
+            print(elem)
+            print(len(elem))
+
+
+
+#############################
+###         Tools         ###
+#############################
+
+"""
+Gets the files names from the directory
+"""
+def get_instances():
+    files = os.listdir(".")
+    instances = []
+    #remove other file
+    for file in files:
+        if file.endswith(".dat"):
+            instances.append(file)
+    return instances
+
+"""
+Read and extract the interesting data from an instance
+"""
+def get_data_instance(instance):
+    #Open the instance's file
+    instanceFile = open(instance, "r")
+    #Read the file and store every line in a list
+    data_file = instanceFile.readlines()
+    #Close the file
+    instanceFile.close()
+    return data_file
+
+
+get_child_info(100, [49, 41, 34, 33, 29, 26, 26, 22, 20, 19], [])
+min_boxes = 10
+for elem in total_result:
+    if len(elem) < min_boxes:
+        min_boxes = len(elem)
+for elem in total_result:
+    if len(elem) == min_boxes:
+        print(elem)
+        print(len(elem))
+print(len(total_result))
